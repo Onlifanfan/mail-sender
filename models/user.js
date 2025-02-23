c1f59ac8c906ec0e64770e0f8537c631403ad453
Onlifanfan <onli.fansv2@gmail.com>
2025-02-23 18:26:23 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,8 +5,19 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: false },
     registrationDate: { type: Date, required: false },
-    isConfirmed: { type: Boolean, required: true }
+    isConfirmed: { type: Boolean, required: true },
+    campaigns: [{
+            campaignName: { type: String, required: true },
+            businessName: { type: String, required: true },
+            industry: { type: String, required: true },
+            numberOfFollowUps: { type: String, required: true },
+            language: { type: String, required: true },
+            targetAudience: { type: String, required: true },
+            creationDate: { type: String, required: true }
+        }
+    ]
 });
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
