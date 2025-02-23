7f5a4d13a9d3a2d0a4c4522a01485c3aab7869d3
Onlifanfan <onli.fansv2@gmail.com>
2025-02-23 11:41:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT === '465',
+    secure: process.env.EMAIL_PORT === '587',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
