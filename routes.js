c117d755e19b66ed32e6429c8acf0635ffcf663e
Onlifanfan <onli.fansv2@gmail.com>
2025-02-23 18:28:59 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,15 +2,30 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
+// Import the utils module (assuming it's in index.js in the same directory)
+const utils = require('./index.js');
 
+// Public routes (no authentication required)
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 router.get('/load', (req, res) => {
-    res.render('loading.ejs')
-})
+    res.render('loading.ejs');
+});
+
+// Protected routes (authentication required)
+router.get('/dashboard', utils.ensureLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+});
+
+// You can add more protected routes here
+// router.get('/profile', utils.ensureLoggedIn, (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'profile.html'));
+// });
+
 module.exports = router;
